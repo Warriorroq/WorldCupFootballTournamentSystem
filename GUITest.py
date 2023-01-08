a19fafdc578de580fefba8ac15159b0f60cdc7e5
Warriorroq <warriorroq@gmail.com>
2023-01-08 08:41:07 +0100

    some renaimings in code

--- GUITest.py
+++ GUITest.py
@@ -3,14 +3,14 @@ import glob
 eel.init("web")
 
 @eel.expose
-def readAllImages():
+def readAllCountryImages():
     images = []
-    for source in [i.replace('web/', '').replace('\\', '/') for i in glob.glob('web/img/*.jpeg')]:
-        countryName = source.replace('img/', '').replace('.jpeg', ' ').replace('_', ' ')
+    for source in [i.replace('web/', '').replace('\\', '/') for i in glob.glob('web/images/country/*.jpeg')]:
+        countryName = source.replace('images/country/', '').replace('.jpeg', ' ').replace('_', ' ')
         images.append((countryName, source))
     return images
 
-print(readAllImages())
+print(readAllCountryImages())
 @eel.expose
 def printMessage(message):
     print(message)
