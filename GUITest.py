bd8032a9d3c78f90c359f5197d73a82c32300f32
Warriorroq <warriorroq@gmail.com>
2023-01-08 10:31:10 +0100

    testing changes

--- GUITest.py
+++ GUITest.py
@@ -2,19 +2,14 @@ import eel
 import glob
 eel.init("web")
 
+
 @eel.expose
 def readAllCountryImages():
     images = []
     for source in [i.replace('web/', '').replace('\\', '/') for i in glob.glob('web/images/country/*.jpeg')]:
-        countryName = source.replace('images/country/', '').replace('.jpeg', ' ').replace('_', ' ')
+        countryName = source.replace('images/country/', '').replace('.jpeg', ' ').replace('_', ' ').strip()
         images.append((countryName, source))
     return images
-
-print(readAllCountryImages())
-@eel.expose
-def printMessage(message):
-    print(message)
-    return message
 
 
 eel.start("index.html", size=(1080, 720))
