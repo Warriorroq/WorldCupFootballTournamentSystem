b70792acf7fdac570d18ab20bbaceb2911e7477f
Warriorroq <warriorroq@gmail.com>
2023-01-08 08:29:49 +0100

    added functions for image reading

--- GUITest.py
+++ GUITest.py
@@ -1,7 +1,16 @@
 import eel
+import glob
 eel.init("web")
 
+@eel.expose
+def readAllImages():
+    images = []
+    for source in [i.replace('web/', '').replace('\\', '/') for i in glob.glob('web/img/*.jpeg')]:
+        countryName = source.replace('img/', '').replace('.jpeg', ' ').replace('_', ' ')
+        images.append((countryName, source))
+    return images
 
+print(readAllImages())
 @eel.expose
 def printMessage(message):
     print(message)
